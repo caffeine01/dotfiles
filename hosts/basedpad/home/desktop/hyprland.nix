5350564898f0ad29079e5544754f9b5d096576e9
caffeine01 <caffeine@caffeine.moe>
2024-12-30 22:00:12 +0000

    skill issue

--- hosts/basedpad/home/desktop/hyprland.nix
+++ hosts/basedpad/home/desktop/hyprland.nix
@@ -1,22 +1,22 @@
 { inputs, pkgs, ... }:
 {
-    wayland.windowManager.hyprland = {
-        plugins = [
-          inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
-        ];
-        settings = {
-          misc = {
-            vfr = true;
-          };
-          monitor = [
-            "LVDS-1,1920x1080@60,0x0,1"          
-          ];
+  wayland.windowManager.hyprland = {
+    plugins = [
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+    ];
+    settings = {
+      misc = {
+        vfr = true;
+      };
+      monitor = [
+        "LVDS-1,1920x1080@60,0x0,1"          
+      ];
 
-          decoration = {
-            shadow.enabled = false;
-            blur.enabled = false;
-            animations.enabled = false;
-          }
+      decoration = {
+        shadow.enabled = false;
+	blur.enabled = false;
+	animations.enabled = false;
       };
     };
+  };
 }
